aab24cdc604f0231eef94909cb5ea63af578caf0
John Ryan <ryjohn@google.com>
2021-03-22 16:16:04 -0700

    remove comment

--- nav2-usability/scenario-code/deeplink-queryparam/router.dart
+++ nav2-usability/scenario-code/deeplink-queryparam/router.dart
@@ -2,9 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// Full sample that shows a custom RouteInformationParser and RouterDelegate
-/// parsing named routes and declaratively building the stack of pages for the
-/// [Navigator].
 import 'package:flutter/material.dart';
 
 void main() {
