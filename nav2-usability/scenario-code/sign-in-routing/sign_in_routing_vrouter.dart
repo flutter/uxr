b20d9cb886811a2ccf280cb02e75454b3a1a38d4
Lulupointu <lucasdelsol@hotmail.fr>
2021-04-01 11:31:40 +0200

    Adding sign-in scenario

---
+++ nav2-usability/scenario-code/sign-in-routing/sign_in_routing_vrouter.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:vrouter/vrouter.dart';
+
+void main() {
+  runApp(BooksApp());
+}
+
+class Credentials {
+  final String username;
+  final String password;
+
+  Credentials(this.username, this.password);
+}
+
+abstract class Authentication {
+  Future<bool> isSignedIn();
+
+  Future<void> signOut();
+
+  Future<bool> signIn(String username, String password);
+}
+
+class MockAuthentication implements Authentication {
+  bool _signedIn = false;
+
+  @override
+  Future<bool> isSignedIn() async {
+    return _signedIn;
+  }
+
+  @override
+  Future<void> signOut() async {
+    _signedIn = false;
+  }
+
+  @override
+  Future<bool> signIn(String username, String password) async {
+    return _signedIn = true;
+  }
+}
+
+class BooksApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BooksAppState();
+}
+
+class AppState {
+  final vRouterKey = GlobalKey<VRouterState>();
+
+  final Authentication auth;
+
+  AppState(this.auth);
+
+  Future<bool> signIn(String username, String password) async {
+    var success = await auth.signIn(username, password);
+    vRouterKey.currentState!.push('/');
+    return success;
+  }
+
+  Future<void> signOut() async {
+    await auth.signOut();
+    vRouterKey.currentState!.push('/signIn');
+  }
+}
+
+class _BooksAppState extends State<BooksApp> {
+  final AppState _appState = AppState(MockAuthentication());
+
+  @override
+  Widget build(BuildContext context) {
+    return VRouter(
+      key: _appState.vRouterKey,
+      routes: [
+        VGuard(
+          beforeEnter: (vRedirector) async =>
+          (await _appState.auth.isSignedIn()) ? vRedirector.push('/') : null,
+          stackedRoutes: [
+            VWidget(
+              path: '/signIn',
+              widget: SignInScreen(
+                onSignedIn: (Credentials credentials) =>
+                    _appState.signIn(credentials.username, credentials.password),
+              ),
+            ),
+          ],
+        ),
+        VGuard(
+          beforeEnter: (vRedirector) async =>
+          (!(await _appState.auth.isSignedIn())) ? vRedirector.push('/signIn') : null,
+          stackedRoutes: [
+            VWidget(
+              path: '/',
+              widget: HomeScreen(onSignOut: _appState.signOut),
+              stackedRoutes: [
+                VWidget(path: 'books', widget: BooksListScreen()),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  final VoidCallback onSignOut;
+
+  HomeScreen({required this.onSignOut});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () => context.vRouter.push('/books'),
+              child: Text('View my bookshelf'),
+            ),
+            ElevatedButton(
+              onPressed: onSignOut,
+              child: Text('Sign out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignInScreen extends StatefulWidget {
+  final ValueChanged<Credentials> onSignedIn;
+
+  SignInScreen({required this.onSignedIn});
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  String _username = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(hintText: 'username (any)'),
+              onChanged: (s) => _username = s,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'password (any)'),
+              obscureText: true,
+              onChanged: (s) => _password = s,
+            ),
+            ElevatedButton(
+              onPressed: () => widget.onSignedIn(Credentials(_username, _password)),
+              child: Text('Sign in'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BooksListScreen extends StatelessWidget {
+  BooksListScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Stranger in a Strange Land'),
+            subtitle: Text('Robert A. Heinlein'),
+          ),
+          ListTile(
+            title: Text('Foundation'),
+            subtitle: Text('Isaac Asimov'),
+          ),
+          ListTile(
+            title: Text('Fahrenheit 451'),
+            subtitle: Text('Ray Bradbury'),
+          ),
+        ],
+      ),
+    );
+  }
+}
