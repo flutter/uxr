9ddf9e47bd20952ebbbb4a976c672ff09cf71a82
Xuan Dai Nguyen <nguyenxndaidev@gmail.com>
2021-04-03 15:57:06 +0200

    Implement deeplink_pathparam_navi.dart

--- nav2-usability/scenario_code/lib/deeplink_pathparam_navi.dart
+++ nav2-usability/scenario_code/lib/deeplink_pathparam_navi.dart
@@ -5,50 +5,27 @@
 // This file is tested with Navi 0.1.0.
 // If it doesn't work with newer version, please check live version at https://github.com/zenonine/navi/tree/master/examples/uxr
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:navi/navi.dart';
 
 void main() {
   runApp(BooksApp());
 }
 
 class Book {
+  final String id;
   final String title;
   final String author;
 
-  Book(this.title, this.author);
+  Book(this.id, this.title, this.author);
 }
 
-class AppState extends ChangeNotifier {
-  final List<Book> _books = [
-    Book('Stranger in a Strange Land', 'Robert A. Heinlein'),
-    Book('Foundation', 'Isaac Asimov'),
-    Book('Fahrenheit 451', 'Ray Bradbury'),
-  ];
-
-  int? _selectedBookId;
-
-  int? get selectedBookId => _selectedBookId;
-
-  set selectedBookId(int? id) {
-    _selectedBookId = id;
-    notifyListeners();
-  }
-
-  Book? get selectedBook {
-    var idx = _selectedBookId;
-    if (idx == null) {
-      return null;
-    }
-    return _books[idx];
-  }
-
-  List<Book> get allBooks => _books;
-
-  void clearSelectedBook() {
-    _selectedBookId = null;
-    notifyListeners();
-  }
-}
+final List<Book> books = [
+  Book('0', 'Stranger in a Strange Land', 'Robert A. Heinlein'),
+  Book('1', 'Foundation', 'Isaac Asimov'),
+  Book('2', 'Fahrenheit 451', 'Ray Bradbury'),
+];
 
 class BooksApp extends StatefulWidget {
   @override
@@ -56,9 +33,9 @@ class BooksApp extends StatefulWidget {
 }
 
 class _BooksAppState extends State<BooksApp> {
-  final BookRouterDelegate _routerDelegate = BookRouterDelegate();
-  final BookRouteInformationParser _routeInformationParser =
-      BookRouteInformationParser();
+  final NaviRouterDelegate _routerDelegate =
+      NaviRouterDelegate.material(rootPage: RootPage());
+  final NaviInformationParser _routeInformationParser = NaviInformationParser();
 
   @override
   void dispose() {
@@ -76,148 +53,11 @@ class _BooksAppState extends State<BooksApp> {
   }
 }
 
-class BookRouteInformationParser extends RouteInformationParser<BookRoutePath> {
-  @override
-  Future<BookRoutePath> parseRouteInformation(
-      RouteInformation routeInformation) async {
-    final uri = Uri.parse(routeInformation.location!);
-    // Handle '/'
-    if (uri.pathSegments.isEmpty) {
-      return BookRoutePath.home();
-    }
-
-    // Handle '/book/:id'
-    if (uri.pathSegments.length == 2) {
-      if (uri.pathSegments[0] != 'book') return BookRoutePath.home();
-      final remaining = uri.pathSegments[1];
-      final id = int.tryParse(remaining);
-      if (id == null) return BookRoutePath.home();
-      return BookRoutePath.details(id);
-    }
-
-    // Handle unknown routes
-    return BookRoutePath.home();
-  }
-
-  @override
-  RouteInformation restoreRouteInformation(BookRoutePath path) {
-    late final String location;
-    if (path.isHomePage) {
-      location = '/';
-    }
-    if (path.isDetailsPage) {
-      location = '/book/${path.id}';
-    }
-    return RouteInformation(location: location);
-  }
-}
-
-class BookRouterDelegate extends RouterDelegate<BookRoutePath>
-    with ChangeNotifier, PopNavigatorRouterDelegateMixin<BookRoutePath> {
-  @override
-  final GlobalKey<NavigatorState> navigatorKey;
-  final AppState _appState = AppState();
-
-  BookRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>() {
-    _appState.addListener(() => notifyListeners());
-  }
-
-  @override
-  void dispose() {
-    _appState.dispose();
-    super.dispose();
-  }
-
-  @override
-  BookRoutePath get currentConfiguration {
-    final id = _appState.selectedBookId;
-    if (id == null) {
-      return BookRoutePath.home();
-    } else {
-      return BookRoutePath.details(id);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final selectedBook = _appState.selectedBook;
-    return Navigator(
-      key: navigatorKey,
-      pages: [
-        MaterialPage(
-          key: ValueKey('BooksListPage'),
-          child: BooksListScreen(
-            books: _appState.allBooks,
-            onTapped: _handleBookTapped,
-          ),
-        ),
-        if (selectedBook != null) BookDetailsPage(book: selectedBook)
-      ],
-      onPopPage: (route, result) {
-        if (!route.didPop(result)) {
-          return false;
-        }
-
-        // Update the list of pages by setting _selectedBook to null
-        _appState.clearSelectedBook();
-
-        return true;
-      },
-    );
-  }
-
-  @override
-  Future<void> setNewRoutePath(BookRoutePath path) async {
-    if (path.isDetailsPage) {
-      final id = path.id;
-      _appState.selectedBookId = id;
-    } else {
-      _appState.clearSelectedBook();
-    }
-  }
-
-  void _handleBookTapped(int bookId) {
-    _appState.selectedBookId = bookId;
-  }
-}
-
-class BookDetailsPage extends Page<dynamic> {
-  final Book book;
-
-  BookDetailsPage({
-    required this.book,
-  }) : super(key: ValueKey(book));
-
-  @override
-  Route<dynamic> createRoute(BuildContext context) {
-    return MaterialPageRoute(
-      settings: this,
-      builder: (BuildContext context) {
-        return BookDetailsScreen(book: book);
-      },
-    );
-  }
-}
-
-class BookRoutePath {
-  final int? id;
-
-  BookRoutePath.home() : id = null;
-
-  BookRoutePath.details(this.id);
-
-  bool get isHomePage => id == null;
-
-  bool get isDetailsPage => id != null;
-}
-
 class BooksListScreen extends StatelessWidget {
   final List<Book> books;
-  final ValueChanged<int> onTapped;
 
   BooksListScreen({
     required this.books,
-    required this.onTapped,
   });
 
   @override
@@ -230,7 +70,7 @@ class BooksListScreen extends StatelessWidget {
             ListTile(
               title: Text(book.title),
               subtitle: Text(book.author),
-              onTap: () => onTapped(books.indexOf(book)),
+              onTap: () => context.navi.stack(BookStackMarker()).state = book,
             )
         ],
       ),
@@ -262,3 +102,35 @@ class BookDetailsScreen extends StatelessWidget {
     );
   }
 }
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RouteStack<Book?>(
+      marker: BookStackMarker(),
+      pages: (context, state) => [
+        MaterialPage<dynamic>(
+          key: ValueKey('Books'),
+          child: BooksListScreen(books: books),
+        ),
+        if (state != null)
+          MaterialPage<dynamic>(
+            key: ValueKey(state.id),
+            child: BookDetailsScreen(book: state),
+          ),
+      ],
+      updateStateOnNewRoute: (routeInfo) {
+        if (routeInfo.hasPrefixes(['book'])) {
+          final bookId = routeInfo.pathSegmentAt(1)?.trim();
+          return books.firstWhereOrNull((book) => book.id == bookId);
+        }
+      },
+      updateRouteOnNewState: (state) => RouteInfo(
+        pathSegments: state != null ? ['book', state.id] : [],
+      ),
+      updateStateBeforePop: (context, route, dynamic result, state) => null,
+    );
+  }
+}
+
+class BookStackMarker extends StackMarker<Book?> {}
