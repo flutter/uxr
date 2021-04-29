23bae8b5bb5e46535458eaf2a55d482a1f2ef11b
John Ryan <ryjohn@google.com>
2021-04-29 13:34:55 -0700

    add generated files and add auto_route to pubspec.yaml

---
+++ nav2-usability/scenario_code/lib/deeplink-pathparam/deeplink_pathparam_auto_route.gr.dart
@@ -0,0 +1,60 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouteGenerator
+// **************************************************************************
+
+import 'package:auto_route/auto_route.dart' as _i1;
+import 'package:flutter/material.dart' as _i2;
+
+import 'deeplink_pathparam_auto_route.dart' as _i3;
+
+class AppRouter extends _i1.RootStackRouter {
+  AppRouter([_i2.GlobalKey<_i2.NavigatorState>? navigatorKey])
+      : super(navigatorKey);
+
+  @override
+  final Map<String, _i1.PageFactory> pagesMap = {
+    BooksListRoute.name: (routeData) {
+      return _i1.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i3.BooksListScreen());
+    },
+    BookDetailsRoute.name: (routeData) {
+      var pathParams = routeData.pathParams;
+      final args = routeData.argsAs<BookDetailsRouteArgs>(
+          orElse: () => BookDetailsRouteArgs(id: pathParams.getInt('id')));
+      return _i1.MaterialPageX<dynamic>(
+          routeData: routeData, child: _i3.BookDetailsScreen(id: args.id));
+    }
+  };
+
+  @override
+  List<_i1.RouteConfig> get routes => [
+        _i1.RouteConfig(BooksListRoute.name, path: '/'),
+        _i1.RouteConfig(BookDetailsRoute.name, path: '/book/:id'),
+        _i1.RouteConfig('*#redirect',
+            path: '*', redirectTo: '/', fullMatch: true)
+      ];
+}
+
+class BooksListRoute extends _i1.PageRouteInfo {
+  const BooksListRoute() : super(name, path: '/');
+
+  static const String name = 'BooksListRoute';
+}
+
+class BookDetailsRoute extends _i1.PageRouteInfo<BookDetailsRouteArgs> {
+  BookDetailsRoute({required int id})
+      : super(name,
+            path: '/book/:id',
+            args: BookDetailsRouteArgs(id: id),
+            params: {'id': id});
+
+  static const String name = 'BookDetailsRoute';
+}
+
+class BookDetailsRouteArgs {
+  const BookDetailsRouteArgs({required this.id});
+
+  final int id;
+}
