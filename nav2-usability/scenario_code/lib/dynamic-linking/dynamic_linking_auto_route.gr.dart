5d16e7b44e65e546ac436f5ba3461a12eb6a6a38
Tao Dong <taodong@google.com>
2021-05-03 16:37:11 -0700

    Fix file names in generated code for auto_route snippets (#60)

--- nav2-usability/scenario_code/lib/dynamic-linking/dynamic_linking_auto_route.gr.dart
+++ nav2-usability/scenario_code/lib/dynamic-linking/dynamic_linking_auto_route.gr.dart
@@ -7,7 +7,7 @@
 import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
-import 'main.dart' as _i3;
+import 'dynamic_linking_auto_route.dart' as _i3;
 
 class AppRouter extends _i1.RootStackRouter {
   AppRouter(
