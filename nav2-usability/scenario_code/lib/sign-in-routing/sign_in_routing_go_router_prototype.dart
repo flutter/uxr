3f9031301032e71bc3d6592d42315f17d1e31302
John Ryan <ryjohn@google.com>
2022-06-02 13:28:33 -0700

    Update comments

--- nav2-usability/scenario_code/lib/sign-in-routing/sign_in_routing_go_router_prototype.dart
+++ nav2-usability/scenario_code/lib/sign-in-routing/sign_in_routing_go_router_prototype.dart
@@ -1,9 +1,6 @@
-// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
+// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
-/// Sign-in example
-/// Done using go_router
 
 import 'package:flutter/material.dart';
 import 'package:go_router_prototype/go_router_prototype.dart';
